acd8e5f6d264c723e5ffa2a0d861c1312bc94660
Sebastian Grygorczuk <stem08992@gmail.com>
2024-10-21 15:58:17 -0400

    10.21

--- Assets/Scripts/PlayerWalking.cs
+++ Assets/Scripts/PlayerWalking.cs
@@ -6,15 +6,28 @@ using UnityEngine;
 public class PlayerWalking : MonoBehaviour
 {
     Rigidbody2D Rigidbody2D;
+    SpriteRenderer spriteRenderer;
     public float Speed = 3; 
     // Start is called before the first frame update
     void Start(){
         Rigidbody2D = GetComponent<Rigidbody2D>(); 
+        spriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update(){
         float xSpeed = Input.GetAxis("Horizontal");
+
+        if (xSpeed > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+
+
 
         Rigidbody2D.velocity = new Vector2(xSpeed * Speed, Rigidbody2D.velocity.y);
     }
