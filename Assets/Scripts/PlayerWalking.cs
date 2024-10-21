a6c08c514a594fd248c908b803a1b38792c37e0c
Sebastian Grygorczuk <stem08992@gmail.com>
2024-10-21 15:58:07 -0400

    10.21

--- Assets/Scripts/PlayerWalking.cs
+++ Assets/Scripts/PlayerWalking.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerWalking : MonoBehaviour
 {
+    Rigidbody2D Rigidbody2D;
+    public float Speed = 3; 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        Rigidbody2D = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        float xSpeed = Input.GetAxis("Horizontal");
+
+        Rigidbody2D.velocity = new Vector2(xSpeed * Speed, Rigidbody2D.velocity.y);
     }
 }
