f7e82414ab3fc9b351c791c7ac79e54a703a714a
Sebastian Grygorczuk <stem08992@gmail.com>
2024-10-28 16:05:23 -0400

    10.28

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -8,7 +8,26 @@ public class PlayerJump : MonoBehaviour
     public float JumpForce = 10;
 
     public Transform feetCollider;
-    public LayerMask groundMask; 
+    public LayerMask groundMask;
+
+    private bool waterCheck;
+    private string waterTag = "Water";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == waterTag)
+        {
+            waterCheck = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == waterTag)
+        {
+            waterCheck = false;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +38,9 @@ public class PlayerJump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        bool groundCheck = Physics2D.OverlapCapsule(feetCollider.position, new Vector2(1, 0.08f), CapsuleDirection2D.Horizontal, 0, groundMask);
+
+        if (Input.GetKeyDown(KeyCode.Space) && (groundCheck || waterCheck))
         {
 
             rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, JumpForce);
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public Transform respawnPoint;
+
+    private string deathTag = "Death";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
